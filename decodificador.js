061f2077c86846902c4d949fce7d0786f78f0647
Débora Silva <fracislayned2@gmail.com>
2024-02-29 17:37:01 -0300

    implementa logica para criptografar e descriptografar texto

--- decodificador.js
+++ decodificador.js
@@ -1,4 +1,61 @@
-function click() {
-    const botaoCriptografar = document.getElementById("botao-criptografar");
-    botaoCriptografar.addEventListener("click", click)
+function criptografar(textoCriptografar) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    let textoCriptLower = textoCriptografar.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        textoCriptLower = textoCriptLower.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+
+    return textoCriptLower;
+}
+
+function descriptografar(textoDescriptografar) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    let textoDescriptLower = textoDescriptografar.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        textoDescriptLower = textoDescriptLower.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    }
+
+    return textoDescriptLower;
+}
+
+
+function onClickEncriptBtn() {
+    const textoCriptografar = document.getElementById("decodificador__input")
+
+    if (textoCriptografar.value !== '') {
+        const divHide = document.getElementsByClassName("hide_div")[0];
+        const divShow = document.getElementsByClassName("show_div")[0];
+        const textArea = document.getElementsByClassName("decodificador__paragrafo")[0];
+
+        console.log(divHide);
+        console.log(divShow);
+
+        divHide.style.display = "block";
+        divShow.style.display = "none";
+
+        const ret = criptografar(textoCriptografar.value);
+        textArea.value = ret;
+    }
+}
+
+function onClickDescriptBtn() {
+    const textoCriptografar = document.getElementById("decodificador__input")
+
+    if (textoCriptografar.value !== '') {
+        const divHide = document.getElementsByClassName("hide_div")[0];
+        const divShow = document.getElementsByClassName("show_div")[0];
+        const textArea = document.getElementsByClassName("decodificador__paragrafo")[0];
+
+        console.log(divHide);
+        console.log(divShow);
+
+        divHide.style.display = "block";
+        divShow.style.display = "none";
+
+        const ret = descriptografar(textoCriptografar.value);
+        textArea.value = ret;
+    }
+
 }
