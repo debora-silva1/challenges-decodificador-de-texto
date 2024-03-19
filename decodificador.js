5b122bae503e9e9b70ea4a7dc87e10acbfa28c1d
debora-silva <francislayned2@gmail.com>
2024-03-19 19:19:47 -0300

    Ajustes

--- decodificador.js
+++ decodificador.js
@@ -1,59 +1,89 @@
 function criptografar(textoCriptografar) {
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    let textoCriptLower = textoCriptografar.toLowerCase()
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],
+    ];
+    let textoCriptLower = textoCriptografar.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-        textoCriptLower = textoCriptLower.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        textoCriptLower = textoCriptLower.replaceAll(
+            matrizCodigo[i][0],
+            matrizCodigo[i][1]
+        );
     }
 
     return textoCriptLower;
 }
 
 function descriptografar(textoDescriptografar) {
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    let textoDescriptLower = textoDescriptografar.toLowerCase()
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],
+    ];
+    let textoDescriptLower = textoDescriptografar.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-        textoDescriptLower = textoDescriptLower.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        textoDescriptLower = textoDescriptLower.replaceAll(
+            matrizCodigo[i][1],
+            matrizCodigo[i][0]
+        );
     }
 
     return textoDescriptLower;
 }
 
-
 function onClickEncriptBtn() {
-    const textoCriptografar = document.getElementById("decodificador__input")
+    const textoCriptografar = document.getElementById("decodificador__input");
 
+    if (textoCriptografar.value !== "") {
+        const divShow = document.getElementsByClassName("show__div")[0];
+        const divHide = document.getElementsByClassName("hide__div")[0];
+        const botaoClicadoCriptografar = document.getElementById("botao-copiar");
+        const pDecoder = document.getElementById("decoder");
 
-    if (textoCriptografar.value !== '') {
-        const divHide = document.getElementsByClassName("show__div")[0];
-        console.log(divHide)
-        const botaoClicadoCriptografar = document.getElementById("botao-copiar")
+        divShow.classList.remove("show__div");
+        divShow.classList.add("hide__div");
 
-        divHide.style.display = "none";
+        divHide.classList.remove("hide__div");
+        divHide.classList.add("show__div");
+
         botaoClicadoCriptografar.style.display = "block";
 
         const ret = criptografar(textoCriptografar.value);
-        textArea.value = ret;
+        const node = document.createTextNode(ret);
+        pDecoder.appendChild(node);
     }
 }
 
 function onClickDescriptBtn() {
-    const textoCriptografar = document.getElementById("decodificador__input")
+    const textoCriptografar = document.getElementById("decodificador__input");
 
-    if (textoCriptografar.value !== '') {
-        const divHide = document.getElementsByClassName("hide_div")[0];
+    if (textoCriptografar.value !== "") {
+        const divShow = document.getElementsByClassName("show__div")[0];
+        const divHide = document.getElementsByClassName("hide__div")[0];
+        const botaoClicadoCriptografar = document.getElementById("botao-copiar");
+        const pDecoder = document.getElementById("decoder");
 
-        divHide.style.display = "none";
+        divShow.classList.remove("show__div");
+        divShow.classList.add("hide__div");
+
+        divHide.classList.remove("hide__div");
+        divHide.classList.add("show__div");
+
+        botaoClicadoCriptografar.style.display = "block";
 
         const ret = descriptografar(textoCriptografar.value);
-        textArea.value = ret;
+        const node = document.createTextNode(ret);
+        pDecoder.appendChild(node);
     }
-
 }
 function onClickCopiarBtn() {
-    const botaoClicadoCriptografar = document.querySelector("botao-criptografar")
-    console.log(botaoClicadoCriptografar)
-
+    const botaoClicadoCriptografar = document.querySelector("botao-criptografar");
+    console.log(botaoClicadoCriptografar);
 }
-
